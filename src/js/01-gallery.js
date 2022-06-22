8fc154ecc630d321425959a02b18bdd7c3b04955
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-06-22 20:29:55 +0300

    save

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,9 +5,7 @@ import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const galleryRef = document.querySelector(".gallery");
-// const galleryImg = document.querySelector(".gallery__image");
-// const galleryLink = document.querySelector(".gallery__link");
+const gallery = document.querySelector(".gallery");
 
 const newGalleryIm = galleryItems
   .map(({ description, original, preview }) => {
@@ -22,17 +20,103 @@ const newGalleryIm = galleryItems
   })
   .join("");
 
-	galleryRef.insertAdjacentHTML("afterbegin", newGalleryIm);
+gallery.insertAdjacentHTML("afterbegin", newGalleryIm);
 
-	// galleryRef.addEventListener("click", openModal);
+  new SimpleLightbox(".gallery a ", {
+    captionsData: "alt",
+    captionClass: "bottom",
+    captionDelay: "250",
+  });
 
-	// function openModal(event) {
-	// 	event.preventDefault();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const galleryRef = document.querySelector(".gallery");
+// // const galleryImg = document.querySelector(".gallery__image");
+// // const galleryLink = document.querySelector(".gallery__link");
+
+// const newGalleryIm = galleryItems
+//   .map(({ description, original, preview }) => {
+//     return `
+// 		<a class="gallery__item" href="${original}">
+// 			<img class="gallery__image" 
+// 					height="600px"
+// 					width="1200px"
+// 					src="${preview}"
+// 					alt="${description}" />
+// 		</a>`;
+//   })
+//   .join("");
+
+// 	galleryRef.insertAdjacentHTML("afterbegin", newGalleryIm);
+
+// 	// galleryRef.addEventListener("click", openModal);
+
+// 	// function openModal(event) {
+// 	// 	event.preventDefault();
 		
-	// 	// const dataSourse = event.target.dataset.source;
+// 	// 	// const dataSourse = event.target.dataset.source;
 		
-	// 	if (event.target.tagName !== "IMG") {
-	// 		return;
-	// 	}
-		new SimpleLightbox('.gallery a ', {captionsData:	'alt' , captionClass:	'bottom', captionDelay:	'250' });
-	// }
+// 	// 	if (event.target.tagName !== "IMG") {
+// 	// 		return;
+// 	// 	}
+// 		new SimpleLightbox('.gallery a ', {captionsData:	'alt' , captionClass:	'bottom', captionDelay:	'250' });
+// 	// }
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,55 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
-// const _ = require('lodash');
-const iframe = document.querySelector('iframe');
+
+const iframe = document.querySelector('#vimeo-player');
+const STOREGY_KEY = 'videoplayer-current-time';
+// let sec;
 
 const player = new Player(iframe);
+console.log(player);
+player.on('timeupdate', throttle(videoOn, 1000))
+function videoOn(e) {
+	const { seconds } = e;
+	JSON.stringify(localStorage.setItem(STOREGY_KEY, seconds));
+}
 
-player.on(
-  'timeupdate',
-  throttle(onTime, 1000)
-);
-function onTime (event) {
-	const { seconds } = event;
-	console.log(seconds);
-	localStorage.setItem('videoplayer-current-time', seconds);
+const a = JSON.parse(localStorage.getItem(STOREGY_KEY));
+console.log(a);
+if(a) {
+	player.setCurrentTime(a);
 }
-const theme = localStorage.getItem('videoplayer-current-time');
-if (theme) {
-	player.setCurrentTime(theme);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const iframe = document.querySelector('iframe');
+
+// const player = new Player(iframe);
+
+// player.on(
+//   'timeupdate',
+//   throttle(onTime, 1000)
+// );
+// function onTime (event) {
+// 	const { seconds } = event;
+// 	console.log(seconds);
+// 	localStorage.setItem('videoplayer-current-time', seconds);
+// }
+// const theme = localStorage.getItem('videoplayer-current-time');
+// if (theme) {
+// 	player.setCurrentTime(theme);
+// }
