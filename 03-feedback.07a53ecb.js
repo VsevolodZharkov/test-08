404b96d081faed2caedbf78dc86be6f20509dffb
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-06-23 14:15:52 +0000

    Deploying to gh-pages from @ VsevolodZharkov/test-08@c20438993b7c681ef620e359f9c4408b853f6741 🚀

---
+++ 03-feedback.07a53ecb.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form");console.log(123);
+//# sourceMappingURL=03-feedback.07a53ecb.js.map
---
+++ 03-feedback.0c2359ce.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form");console.log(123)}();
+//# sourceMappingURL=03-feedback.0c2359ce.js.map
