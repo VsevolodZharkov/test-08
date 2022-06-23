b6b7f0d6303e9840c9f373bef87efab6166d3db5
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-06-23 14:26:46 +0000

    Deploying to gh-pages from @ VsevolodZharkov/test-08@5d3989217cb8120baab9458f5fdcf30fe5f80669 🚀

--- 03-feedback.3e694a14.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".submit");console.log(o),o.addEventListener("click",(function(){console.log(1)}))}();
-//# sourceMappingURL=03-feedback.3e694a14.js.map
--- 03-feedback.bce4b844.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".submit");console.log(e),e.addEventListener("click",(()=>{console.log(1)}));
-//# sourceMappingURL=03-feedback.bce4b844.js.map
