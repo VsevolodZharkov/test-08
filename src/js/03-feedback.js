2d0482ab8813374d41cb30e3c350cc0e65ea1cac
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-06-23 17:32:56 +0300

    x

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,118 +1,65 @@
-// import throttle from "lodash.throttle";
+import throttle from "lodash.throttle";
 
-console.log(1);
+const formRef = document.querySelector('.feedback-form');
 
+const STORAGE_KEY = 'feedback-form-state';
 
+const inputHandler = e => {
+  const { name, value } = e.target;
 
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
 
+  const formData = {
+    ...parsedData,
+    [name]: value,
+  };
 
+  const serlizedData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, serlizedData);
+};
 
+const rehydrateData = () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
 
+  if (!parsedData) {
+    return;
+  }
 
+  const inputNames = Object.keys(parsedData);
 
+  inputNames.forEach(inputName => {
+    const input = formRef.elements[inputName];
 
+    input.value = parsedData[inputName];
+  });
+};
 
+rehydrateData();
 
+const submitHandler = e => {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const formData = new FormData(form);
+  const finalData = {};
 
+  for (const [key, value] of formData.entries()) {
+    if (!value) {
+      alert('please fill in all the fields!');
+      return;
+    }
+    finalData[key] = value;
+  }
 
+  console.log(finalData);
 
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const formRef = document.querySelector('.feedback-form');
-
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const inputHandler = e => {
-//   const { name, value } = e.target;
-
-//   const savedData = localStorage.getItem(STORAGE_KEY);
-//   const parsedData = JSON.parse(savedData);
-
-//   const formData = {
-//     ...parsedData,
-//     [name]: value,
-//   };
-
-//   const serlizedData = JSON.stringify(formData);
-//   localStorage.setItem(STORAGE_KEY, serlizedData);
-// };
-
-// const rehydrateData = () => {
-//   const savedData = localStorage.getItem(STORAGE_KEY);
-//   const parsedData = JSON.parse(savedData);
-
-//   if (!parsedData) {
-//     return;
-//   }
-
-//   const inputNames = Object.keys(parsedData);
-
-//   inputNames.forEach(inputName => {
-//     const input = formRef.elements[inputName];
-
-//     input.value = parsedData[inputName];
-//   });
-// };
-
-// rehydrateData();
-
-// const submitHandler = e => {
-//   e.preventDefault();
-//   const form = e.currentTarget;
-//   const formData = new FormData(form);
-//   const finalData = {};
-
-//   for (const [key, value] of formData.entries()) {
-//     if (!value) {
-//       alert('please fill in all the fields!');
-//       return;
-//     }
-//     finalData[key] = value;
-//   }
-
-//   console.log(finalData);
-
-//   form.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// };
-
-// formRef.addEventListener('input', throttle((evt) => {
-// 	inputHandler(evt)
-// }, 500)
-// );
-// formRef.addEventListener('submit', submitHandler);
+formRef.addEventListener('input', throttle((evt) => {
+	inputHandler(evt)
+}, 500)
+);
+formRef.addEventListener('submit', submitHandler);
